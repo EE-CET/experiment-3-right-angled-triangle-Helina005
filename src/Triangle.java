a969d68a477adb27ce4224871450585eafd2cb2a
Helina Alex <helinatalex@gmail.com>
2026-03-28 10:16:20 +0530

    Triangle

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,19 @@
 public class Triangle {
-    //Add your programs here
+    public static void main(String[] args) {
+
+        int height = 3;
+
+        for (int i = 1; i <= height; i++) {
+
+            int stars = 2 * i - 1; // 1, 3, 5...
+
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+                if (j < stars) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
